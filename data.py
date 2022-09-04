31c193707ae69becf4ea6dec8a30a735f08dc212
Kostas4S <81780796+Kostas4S@users.noreply.github.com>
2022-09-04 15:40:36 -0400

    Add files via upload

---
+++ data.py
@@ -0,0 +1,1 @@
+data = [{'': 'Star_name', '0': 'Sun', '1': 'Sirius', '2': 'Canopus', '3': 'Alpha Centauri', '4': 'Arcturus', '5': 'Vega', '6': 'Capella', '7': 'Rigel', '8': 'Procyon', '9': 'Betelgeuse', '10': 'Achernar'}]
